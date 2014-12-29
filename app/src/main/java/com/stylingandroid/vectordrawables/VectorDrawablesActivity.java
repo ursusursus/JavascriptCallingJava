b4767fe0e0a7b41323adbfd77845ed1ec24822c3
Mark Allison <mark@stylingandroid.com>
2014-12-29 15:26:23 +0000

    Code for Part 1

---
+++ app/src/main/java/com/stylingandroid/vectordrawables/VectorDrawablesActivity.java
@@ -0,0 +1,13 @@
+package com.stylingandroid.vectordrawables;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VectorDrawablesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vector_drawables);
+    }
+}
